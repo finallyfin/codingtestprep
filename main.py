62349595ee9bdf9f9d0f1f535582da8dc6926587
finallyfin <97354925+finallyfin@users.noreply.github.com>
2022-03-22 10:10:31 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+tempGroup = 0;
+totalGroup = 0;
+
+group = input().split();
+group.sort();
+
+for i in range(len(group)):
+  personFearLvl = int(group[i])
+  if personFearLvl > tempGroup:
+    tempGroup += 1
+  elif personFearLvl == tempGroup:
+    totalGroup += 1
+    tempGroup = 1
+
+print(totalGroup)
+
+
+
+
