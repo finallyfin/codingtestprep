d4b6695514c6a8dd95ddfead0bb2eec330e4244e
finallyfin <97354925+finallyfin@users.noreply.github.com>
2022-04-16 14:56:45 +0000

    모범답안

--- main.py
+++ main.py
@@ -35,7 +35,23 @@ print(totalGroup)
 exit status 1
   */
 
+//아래는 모범답안
+import sys
 
+n = int(input())
+phobia = list(map(int, input().split()))
+phobia.sort()
+
+group = 0  # 그룹에 포함되는 모험가 수 세기
+count = 0  # 형성되는 그룹 수 세기
+
+for p in phobia:
+    group += 1
+    if group >= p:
+        count += 1
+        group = 0
+
+print(count)
 
 
 
